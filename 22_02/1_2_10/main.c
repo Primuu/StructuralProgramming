618ee96838310a0e1b72c30e412e763460c1687a
Adam Trentowski <trentowski.adam@gmail.com>
2021-02-22 14:23:32 +0100

    end

---
+++ 22_02/1_2_10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double l;
+    scanf("%lf",&l);
+    printf("%lf",fabs(l));
+
+    return 0;
+}
---
+++ 22_02/1_2_8/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d\n%d\n%d",&a,&b,&c);
+    printf("%f",(double)(a+b+c)/3);
+    return 0;
+}
