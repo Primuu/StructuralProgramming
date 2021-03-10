11c0013ee908e13c24094c8aac1b86faecd74394
Adam Trentowski <trentowski.adam@gmail.com>
2021-03-10 18:04:40 +0100

    1_3_4 + 1_3_7 corrected

--- 01_03/1_3_4/main.c
+++ 01_03/1_3_4/main.c
@@ -3,9 +3,16 @@
 
 int main()
 {
-    int a,b;
-    scanf("%i\n%i",&a,&b);
-    if(abs(a) >= abs(b)) printf("%i",a);
+    int a, b, absa, absb;
+    scanf("%i\n%i", &a, &b);
+
+    if ( a < 0) absa = -a;
+    else absa = a;
+
+    if ( b < 0) absb = -b;
+    else absb = b;
+
+    if(absa >= absb) printf("%i",a);
         else printf("%i",b);
     return 0;
 }
--- 01_03/1_3_7/main.c
+++ 01_03/1_3_7/main.c
@@ -4,23 +4,30 @@
 
 int main()
 {
-    float a,b,c,delta,x1,x2;
+    float a, b, c, delta, x1, x2;
 
-    printf("Podaj wspolczynniki rownania kwadratowego:\n");
+    printf("Podaj wspolczynniki rownania: ");
     scanf("%f\n%f\n%f",&a,&b,&c);
 
-    delta = b * b - 4 * a * c;
+
     if(a != 0){
+                delta = b * b - 4 * a * c;
                 if(delta > 0){
-                    x1 = (-1 * b - sqrt(delta)) / 2 * a;
-                    x2 = (-1 * b + sqrt(delta)) / 2 * a;
-                    printf("x1 = %f\nx2 = %f",x1,x2);
-                    } else if (delta == 0)
-                        {
-                            x1 = -1 * b / 2 * a;
-                            printf("x1 = %f\n",x1);
-                        } else printf("Nie ma rozwiazan");
-    } else printf("To nie jest rownanie kwadratowe");
+                                x1 = (-1 * b - sqrt(delta)) / 2 * a;
+                                x2 = (-1 * b + sqrt(delta)) / 2 * a;
+                                printf("x1 = %f\nx2 = %f",x1,x2);
+                              } else if (delta == 0)
+                                                    {
+                                                     x1 = -1 * b / 2 * a;
+                                                     printf("x1 = %f\n",x1);
+                                                     } else printf("Nie ma rozwiazan rzeczywistych");
+                } else if (b != 0)
+                                    {
+                                     x1 = -c / b;
+                                     printf("x1 = %f\n",x1);
+                                    }
+                                     else printf("Sprzecznosc.");
+
 
 
     return 0;
