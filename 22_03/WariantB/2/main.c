4b7acca1735364013eed9371f12f70564caae89a
Adam Trentowski <trentowski.adam@gmail.com>
2021-03-28 23:05:26 +0200

    Homework_#5_corrected2

--- 22_03/WariantB/2/main.c
+++ 22_03/WariantB/2/main.c
@@ -4,18 +4,23 @@
 int foo(int n)
 {
     if (n == 0 || n == 1) return 3;
-    if (n % 2 == 0) return foo((n / 2) - 1) + n;
-    return 2 * foo(n - 1) - 2 * n;
+    if (n % 2 == 0) return foo((n / 2) - 1) + n / 2;
+    return 2 * foo(n - 1) - (n - 1);
 
     return 0;
 }
 
 int main()
 {
-    int n = 19;
+    int n = 0;
     printf("%i\n", foo(n));
 
-    printf("%i\n", foo(9));
+    printf("%i\n", foo(1));
+    printf("%i\n", foo(2));
+    printf("%i\n", foo(3));
+    printf("%i\n", foo(4));
+    printf("%i\n", foo(5));
+    printf("%i\n", foo(6));
 
     return 0;
 }
